34fd3d7c4168cd58abb8d33ab6c283bd3f879d4b
Santi <xxsmating@gmail.com>
2023-04-23 06:54:57 -0300

    desafío de edades

---
+++ edades.py
@@ -0,0 +1,7 @@
+anio = {1990, 2003, 2010, 1985};
+
+for i in anio:
+    if 2023-i >= 18:
+        print(f"Este chico/a puede pasar, tiene {2023-i} anios y nacio en {i}");
+    else:
+        print(f"Este chico/a no puede pasar, tiene {2023-i} anios y nacio en {i}");
